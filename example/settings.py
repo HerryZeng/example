efe9b6ab6fec614be567268375d53891005f4e53
Herry <herry.zeng@gmail.com>
2018-06-02 23:08:48 +0800

    Update Settings ALLOWED_HOSTS

--- example/settings.py
+++ example/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'xz*h*j2yts2dnr-_rz_nd2v6wsu2e39r&cnag+$)u4b=t8%06v'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
